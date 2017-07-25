3d0dc1370cd7d8d05e04946a3d963b36a92417de
sriram <sriramvasu126@gmail.com>
2017-07-25 17:25:58 -0400

    new

--- gan.py
+++ gan.py
@@ -4,7 +4,7 @@ from image_reader import image_reader
 import os
 from utils_mod import *
 import h5py
-
+import fnmatch
 
 class DCGAN():
 	def __init__(self,keep_prob=0.5,is_gpu=False,weights_path=None,pretrained=False):
@@ -21,7 +21,7 @@ class DCGAN():
 		# self.x=x;
 		self.z=z
 		self.is_training=is_training
-		self.batch_size_z=self.z.get_shape().as_list()
+		self.batch_size_z=self.z.get_shape().as_list()[0]
 		self.reuse=reuse
 		self.gen_out=self.Generator(self.z)
 		self.disc_out_generated=self.Discriminator(self.gen_out)
@@ -30,55 +30,62 @@ class DCGAN():
 			self.disc_out_images=self.Discriminator(self.x)
 		# self.disc_out_images
 
-	def train(self,x,is_training=True,reuse=False):
+	def train(self,x,is_training=True,reuse=True,gen_lr=1e-5,disc_lr=1e-5):
 		self.x=x
-		self.batch_size_x=self.x.get_shape().as_list()
+		self.batch_size_x=self.x.get_shape().as_list()[0]
+		self.gen_lr=gen_lr
+		self.disc_lr=disc_lr
 		self.is_training=is_training
 		self.reuse=reuse
 		self.disc_out_images=self.Discriminator(self.x)
 		self.gen_loss=self.calc_loss_generator()
 		self.disc_loss=self.calc_loss_discriminator()
 
-		disc_var_names=fnmatch.filter([v.name for v in tf.trainable_variables()],'discriminator*')
-		self.disc_vars=[tf.get_variable(name=name) for name in disc_var_names]
+		disc_var_names=fnmatch.filter([v.name.split(':')[0] for v in tf.trainable_variables()],'discriminator*')
+		gen_var_names=fnmatch.filter([v.name.split(':')[0] for v in tf.trainable_variables()],'generator*')
 
-		gen_var_names=fnmatch.filter([v.name for v in tf.trainable_variables()],'generator*')
-		self.gen_vars=[tf.get_variable(name=name) for name in gen_var_names]
+		print disc_var_names
+		with tf.variable_scope('',reuse=True):
+			self.disc_vars=[tf.get_variable(name=name) for name in disc_var_names]
+		with tf.variable_scope('',reuse=True):
+			self.gen_vars=[tf.get_variable(name=name) for name in gen_var_names]
 
+		# self.gen_vars=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,'generator')
+		# self.disc_vars=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,'discriminator')
 		self.train_discriminator()
 		self.train_generator()
 
 
 
-	def train_discriminator():
-		opt = tf.train.AdamOptimizer(learning_rate)
+	def train_discriminator(self):
+		opt = tf.train.AdamOptimizer(self.disc_lr)
 		gradvar_list=opt.compute_gradients(self.disc_loss,var_list=self.disc_vars)
 		self.disc_train_op=opt.apply_gradients(gradvar_list)
 
 		
-	def train_generator():
-		opt = tf.train.AdamOptimizer(learning_rate)
+	def train_generator(self):
+		opt = tf.train.AdamOptimizer(self.gen_lr)
 		gradvar_list=opt.compute_gradients(self.gen_loss,var_list=self.gen_vars)
 		self.gen_train_op=opt.apply_gradients(gradvar_list)
 
 
 	def calc_loss_generator(self,num_classes=2,weighted=False,weights=None):
 		epsilon = tf.constant(value=1e-10)
-		gen_labels=tf.ones([self.batch_size_z])
+		gen_labels=tf.ones([self.batch_size_z],dtype=tf.int32)
 		gen_logits=self.disc_out_generated
 
-		gen_labels=tf.reshape(tf.one_hot(tf.reshape(labels,[-1]),num_classes),[-1,num_classes]);
-		gen_logits=tf.reshape(logits,[-1,num_classes]);
+		gen_labels=tf.reshape(tf.one_hot(tf.reshape(gen_labels,[-1]),num_classes),[-1,num_classes]);
+		gen_logits=tf.reshape(gen_logits,[-1,num_classes]);
 		if(weighted==False):
 			# cross_entropy=tf.nn.softmax_cross_entropy_with_logits(labels=labels,logits=logits,name='cross_entropy');
 			# cross_entropy_mean=tf.reduce_mean(cross_entropy);
-			softmax=tf.nn.softmax(logits+epsilon)
-			cross_entropy=-1*tf.reduce_sum(tf.mul(labels*tf.log(softmax+epsilon),weights.reshape([num_classes])),axis=1)
+			softmax=tf.nn.softmax(gen_logits+epsilon)
+			cross_entropy=-1*tf.reduce_sum(gen_labels*tf.log(softmax+epsilon),axis=1)
 			cross_entropy_mean=tf.reduce_mean(cross_entropy)
 
 		else:
-			softmax=tf.nn.softmax(logits+epsilon)
-			cross_entropy=-1*tf.reduce_sum(tf.mul(labels*tf.log(softmax+epsilon),weights.reshape([num_classes])),axis=1)
+			softmax=tf.nn.softmax(gen_logits+epsilon)
+			cross_entropy=-1*tf.reduce_sum(tf.multiply(gen_labels*tf.log(softmax+epsilon),weights.reshape([num_classes])),axis=1)
 			cross_entropy_mean=tf.reduce_mean(cross_entropy)
 		tf.add_to_collection('losses', cross_entropy_mean)
 		loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
@@ -87,21 +94,21 @@ class DCGAN():
 
 	def calc_loss_discriminator(self,num_classes=2,weighted=False,weights=None):
 		epsilon = tf.constant(value=1e-10)
-		disc_labels=tf.concat([tf.zeros([self.batch_size_z]),tf.ones([self.batch_size_x])],0)
-		disc_logits=tf.concat([self.disc_out_generated,self.disc_out_images])
+		disc_labels=tf.concat([tf.zeros([self.batch_size_z],dtype=tf.int32),tf.ones([self.batch_size_x],dtype=tf.int32)],0)
+		disc_logits=tf.concat([self.disc_out_generated,self.disc_out_images],0)
 
 		disc_labels=tf.reshape(tf.one_hot(tf.reshape(disc_labels,[-1]),num_classes),[-1,num_classes])
-		disc_output=tf.reshape(disc_output,[-1,num_classes])
+		disc_logits=tf.reshape(disc_logits,[-1,num_classes])
 		if(weighted==False):
 			# cross_entropy=tf.nn.softmax_cross_entropy_with_logits(labels=labels,logits=logits,name='cross_entropy');
 			# cross_entropy_mean=tf.reduce_mean(cross_entropy);
-			softmax=tf.nn.softmax(disc_output+epsilon)
+			softmax=tf.nn.softmax(disc_logits+epsilon)
 			cross_entropy=-1*tf.reduce_sum(disc_labels*tf.log(softmax+epsilon),axis=1)
 			cross_entropy_mean=tf.reduce_mean(cross_entropy)
 
 		else:
-			softmax=tf.nn.softmax(disc_output+epsilon)
-			cross_entropy=-1*tf.reduce_sum(tf.mul(disc_labels*tf.log(softmax+epsilon),weights.reshape([num_classes])),axis=1)
+			softmax=tf.nn.softmax(disc_logits+epsilon)
+			cross_entropy=-1*tf.reduce_sum(tf.multiply(disc_labels*tf.log(softmax+epsilon),weights.reshape([num_classes])),axis=1)
 			cross_entropy_mean=tf.reduce_mean(cross_entropy)
 		tf.add_to_collection('losses', cross_entropy_mean)
 		loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
@@ -110,6 +117,7 @@ class DCGAN():
 
 	def Generator(self,gen_input):
 		self.gen_input=gen_input
+		gi=tf.reshape(self.gen_input,[])
 		# generator input shape bsx100
 		# generator output shape bsxhxw
 		with tf.variable_scope('generator'):
@@ -184,17 +192,34 @@ class DCGAN():
 
 
 def train_DCGAN():
-	f=h5py.File('/home/sriram/intern/mnist.h5','r')
+	f=h5py.File('mnist.h5','r')
 	x_train=f['x_train'][:]
 	y_train=f['y_train'][:]
 	batch_size=5
+	rand_vect_size=100
+	gen_base_lr=1e-5
+	disc_base_lr=1e-5
 	train_data=tf.placeholder(dtype=tf.float32,shape=[batch_size,112,112,1])
-	random_vector=tf.placeholder(dtype=tf.float32,shape=[batch_size,100])
+	random_vector=tf.placeholder(dtype=tf.float32,shape=[batch_size,rand_vect_size])
 	train_labels=tf.placeholder(dtype=tf.int32,shape=[batch_size])
+	count=tf.placeholder(dtype=tf.int32)
 
 	gan=DCGAN()
 	gan.inference(random_vector,reuse=False)
-	gan.train(train_data,is_training=False,reuse=True)
+	gen_lr=tf.train.exponential_decay(gen_base_lr,count,1,0.5)
+	disc_lr=tf.train.exponential_decay(disc_base_lr,count,1,0.5)
+	gan.train(train_data,gen_lr=gen_lr,disc_lr=disc_lr,is_training=False,reuse=True)
+	n_epochs=100
+	n_batches=x_train.shape[0]/batch_size
+	sess=tf.Session()
+	sess.run(tf.global_variables_initializer())
+	for i in range(n_epochs):
+		for j in range(n_batches):
+			feed_dict={random_vector:np.random.standard_normal([batch_size,100]),train_data:next_batch(x_train,batch_size),count=0}
+			_=sess.run(gan.disc_train_op,feed_dict)
+			_=sess.run(gan.gen_train_op,feed_dict)
+			print 'epoch:',i,'batch:',j
+			
 
 
 
