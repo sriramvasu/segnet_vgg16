baf75987ffc681cddf77d6d90e6a7cc33e831633
Maggie <mwigness@cs.colostate.edu>
2017-08-09 13:53:16 -0400

    changes

--- segnet_train_top.py
+++ segnet_train_top.py
@@ -350,7 +350,9 @@ def train_segnet():
 	net.match_labels=match_labels
 
 	net.loss=net.calc_loss(train_logits,train_labels,net.num_classes)
-	learning_rate=tf.train.exponential_decay(base_lr,count,1,1.0/rate)
+	# learning_rate=tf.train.exponential_decay(base_lr,count,1,1.0/rate)
+	learning_rate = base_lr*(1/rate)^(count/1)
+
 	net.train(learning_rate)
 	prediction_train=tf.argmax(train_logits,axis=3)
 	prediction_valid=tf.argmax(valid_logits,axis=3)
